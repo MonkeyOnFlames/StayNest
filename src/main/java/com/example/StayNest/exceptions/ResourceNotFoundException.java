6467219c4d0e8fb8d9820365528caf28407915d2
Adam Alkaffagi <adam_1986@live.se>
2025-02-24 15:59:42 +0100

    fixed CRUD operations, need to be reviewed

--- src/main/java/com/example/StayNest/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.StayNest.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/StayNest/exceptions/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.StayNest.exceptions;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
