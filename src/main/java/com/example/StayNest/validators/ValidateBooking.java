bfa742281a97dfa0aacea410b0f15abd6299834b
denlillamurmeln <rooba87@gmail.com>
2025-09-12 09:14:56 +0200

    started with ValidateBooking

--- src/main/java/com/example/StayNest/validators/ValidateBooking.java
+++ src/main/java/com/example/StayNest/validators/ValidateBooking.java
@@ -1,4 +1,12 @@
 package com.example.StayNest.validators;
 
-public class ValidateBooking {
+public class ValidateBooking extends RequestValidator{
+    public ValidateBooking(int validate) {
+        this.validate = validate;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("BOOKING: " + message);
+    }
 }
