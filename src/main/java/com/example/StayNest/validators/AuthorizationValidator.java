5773310b622632393e9c8a7929f280379d68891d
denlillamurmeln <rooba87@gmail.com>
2025-09-19 14:12:47 +0200

    new branch created

--- src/main/java/com/example/StayNest/validators/AuthorizationValidator.java
+++ src/main/java/com/example/StayNest/validators/AuthorizationValidator.java
@@ -1,5 +1,17 @@
 package com.example.StayNest.validators;
 
-public class AuthorizationValidator extends RequestValidator {
+import com.example.StayNest.helpClasses.validatorChain.ValidateAvailability;
 
+public class AuthorizationValidator extends RequestValidator {
+    @Override
+    public void validationHandler () {
+        ValidateAvailability validateAvailability = new ValidateAvailability(listingRepository);
+        validate.setAvailable(validateAvailability.validateAvailability(booking));
+
+        if (validate.isAvailable()) {
+            super.validationHandler();
+        } else {
+            throw new IllegalArgumentException("The selected dates are not available for booking");
+        }
+    }
 }
